1bd89e3aa993424bb9418a223171a735f543b3db
Alnurr2 <bekmurzaalnur9@gmail.com>
2026-01-21 07:56:32 +0500

    Added strings example

---
+++ Practice1/python_examples/strings.py
@@ -0,0 +1,27 @@
+print("Hello")
+print('Hello')
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+
+a = "Hello, World!"
+print(a[1])
+
+for x in "banana":
+  print(x)
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
