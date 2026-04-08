210e33f9eec4fdfd280e76f994842ff1b90a4d1d
Alnurr2 <bekmurzaalnur9@gmail.com>
2026-04-08 12:58:39 +0500

    added 8 practice

---
+++ Practice8/phonebook.py
@@ -0,0 +1,174 @@
+import psycopg2
+import csv
+from config import load_config
+
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="phonebook",
+    user="postgres",
+    password="Qwerty5656"
+)
+#Creating the PhoneBook Table
+def create_tables():
+    command = """
+CREATE TABLE IF NOT EXIST phonebook (
+               phone_id SERIAL PRIMARY KEY,
+               phone VARCHAR(16) NOT NULL,
+               phone_owner VARCHAR(255) NOT NULL
+);
+""" 
+    with conn.cursor() as cur:
+        cur.execute(command)
+        conn.commit()
+#Adding Contact function
+def insert_contact():
+    name = input("Enter name: ")
+    phone = input("Enter phone: ")
+
+    cur = conn.cursor()
+    
+    cur.execute(
+        "INSERT INTO phonebook (phone,phone_owner) VALUES (%s, %s)",
+        (phone, name)
+    )
+
+    conn.commit()
+    print("Contact added.")
+    cur.close()
+
+
+#Updating contact
+def update_contact():
+    name = input("Enter name to update: ")
+    phone = input("New phone: ")
+
+
+    cur = conn.cursor()
+
+    cur.execute(
+        "UPDATE phonebook SET phone = %s WHERE phone_owner = %s",
+        (phone, name)
+    )
+
+    conn.commit()
+    cur.close()
+
+#Searching
+def search_contact():
+    keyword = input("Search name: ")
+
+
+    cur = conn.cursor()
+
+    cur.execute(
+        "SELECT * FROM phonebook WHERE phone_owner ILIKE %s",
+        (f"%{keyword}%",)
+    )
+
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    if len(rows) == 0:
+       print("Didnt find any contact with that name.")
+    conn.commit()
+    cur.close()
+
+#Deleting
+def delete_contact():
+    name = input("Enter name to delete: ")
+
+    cur = conn.cursor()
+
+    cur.execute(
+        "DELETE FROM phonebook WHERE phone_owner = %s",
+        (name,)
+    )
+
+    
+
+    conn.commit()
+    print("Contact has been deleted.")
+    cur.close()
+
+#Show all
+def showall():
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM phonebook;")
+    rows = cur.fetchall()
+    if len(rows) == 0:
+       print("No contacts yet.")
+    for row in rows:
+        print(row)
+    cur.close() 
+
+#Exporting to csv
+def exporting_to_csv(file):
+    cur = conn.cursor()
+
+    cur.execute("SELECT phone, phone_owner FROM phonebook;")
+    rows = cur.fetchall()
+
+    with open(file, 'w', newline='') as f:
+        import csv
+        writer = csv.writer(f)
+        writer.writerow(["phone", "phone_owner"])
+        writer.writerows(rows)
+    
+    conn.commit()
+    cur.close()
+
+#Importing from csv
+def importing_to_csv(file):
+    cur = conn.cursor()
+
+
+    with open(file, 'r', newline='') as f:
+        import csv
+        reader= csv.reader(f)
+        next(reader)
+        for row in reader:
+           if not row or len(row) < 2:
+            continue
+           cur.execute("INSERT INTO phonebook (phone,phone_owner) VALUES (%s,%s)", row)
+
+    conn.commit()
+    cur.close()
+
+
+def menu():
+    while True:
+        print("------------Phonebook-----------")
+        print("1.Show the contacts")
+        print("2.Add contact")
+        print("3.Deleting contact")
+        print("4.Update the contact")
+        print("5.Search contact")
+        print("6.export to csv")
+        print("7.import from csv")
+        print("0.Exit")
+
+
+        choice = input("Choose:")
+
+        if choice == "1":
+         showall()
+        elif choice == "2":
+         insert_contact()
+        elif choice == "3":
+         delete_contact()
+        elif choice == "4":
+         update_contact()
+        elif choice == "5":
+         search_contact()
+        elif choice == "6":
+         exporting_to_csv("contacts.csv")
+        elif choice == "7":
+         importing_to_csv("import.csv")
+        elif choice == "0":
+         conn.close()
+         break
+
+if __name__=="__main__":
+   menu()
