848dd41107f37f20f2cfee926ec4591dd28527e7
Alnurr2 <bekmurzaalnur9@gmail.com>
2026-04-14 23:49:53 +0500

    added third game

--- Practice09/Balls/main.py
+++ Practice09/Balls/main.py
@@ -1,53 +1,53 @@
 import pygame
-from datetime import datetime
 
-wtime = datetime.now()
-print(wtime)
+
+
 pygame.init()
-WIDTH = 800
-HEIGHT = 600
+WIDTH = 1200
+HEIGHT = 800
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-
-
-angle = 180
-dt = clock.tick(60) / 1000.0
-sample_img = pygame.image.load("mickey.png").convert_alpha()
-smaller_img = pygame.transform.scale(hand1, (200, 200))
-
-image_height = smaller_img.get_height()
-hand_length = image_height // 2
-
-
+done = False
+x1 = 400
+y1 = 300
+clock = pygame.time.Clock()
 while not done:
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+               x1 -= 20
+        if keys[pygame.K_RIGHT]:
+               x1 += 20
+        if keys[pygame.K_UP]:
+                y1 -= 20
+        if keys[pygame.K_DOWN]:
+               y1 += 20
+
+        if x1 + 25 >= WIDTH:
+                x1 -=20
+        elif x1 - 25 <= 0:
+                x1 +=20
+        if y1 + 25 >= HEIGHT:
+                y1 -=20
+        elif y1 -25 <= 0:
+                y1+=20
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         done = True
+              
+              
+              
+              
+              
+              
+              
+              
         
-
-        keys = pygame.key.get_pressed()
-        angle-= 50* dt
-        
-        counter = pygame.time.get_ticks()//1000
-        counter1 = counter / 60
-        seconds = my_font.render(f"seconds: {counter}s", True, (0, 0, 0))
-        minutes = my_font.render(f"minutes: {counter1}", True, (0, 0, 0))
-
-        rotated = pygame.transform.rotate(smaller_img,angle)
-
-        rotated_offset = offset.rotate(-angle)
-        rect = rotated.get_rect(center=(PIVOT_X + rotated_offset.x, PIVOT_Y + rotated_offset.y))
-        rect1 = rotated.get_rect(center=(WIDTH // 2, HEIGHT // 2))
-        center1 = sample_img.get_rect(center=(WIDTH // 2, HEIGHT // 2))
-       
+                       
+                        
         screen.fill((255, 255, 255))
-        mickey_rect = sample_img.get_rect(center=(WIDTH // 2 + 30, HEIGHT // 2 + 20))
-        screen.blit(sample_img,mickey_rect)
-        screen.blit(rotated,rect)
-        screen.blit(seconds, (10, 10))
-        screen.blit(minutes, (10, 30))
+        ball = pygame.draw.circle(screen, (255,0,0),(x1,y1), 25, width=0)
 
+      
+      
+        clock.tick(144)
         pygame.display.flip()
-
-        clock.tick(60)
-        print(counter1)
