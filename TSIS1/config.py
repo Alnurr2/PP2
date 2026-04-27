55b871bfa8e04879cc2eea37feb764a4ee63307f
Alnurr2 <bekmurzaalnur9@gmail.com>
2026-04-27 22:53:27 +0500

    Added tsis

---
+++ TSIS1/config.py
@@ -0,0 +1,7 @@
+DB_CONFIG = {
+    "host": "localhost",
+    "database": "phonebook1",
+    "user": "postgres",
+    "password": "Qwerty5656",
+    "port": 5432
+}
---
+++ TSIS4/config.py
@@ -0,0 +1,7 @@
+DB_CONFIG = {
+    "host": "localhost",
+    "database": "snake",
+    "user": "postgres",
+    "password": "Qwerty5656",
+    "port": 5432
+}
